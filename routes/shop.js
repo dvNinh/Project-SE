1506dbf6594313c3d0684c2f19f22cf563a46603
Đinh Văn Ninh <21020362@vnu.edu.vn>
2023-04-05 21:49:51 +0700

    thêm giao diên trang chủ

--- routes/shop.js
+++ routes/shop.js
@@ -5,6 +5,7 @@ const shopController = require('../controllers/ShopController');
 router.get('/home', shopController.getHome);
 router.get('/shop/create', shopController.create);
 router.post('/shop/store', shopController.store);
+router.get('/products/:slug', shopController.getProduct);
 router.get('/', shopController.getHome);
 
 module.exports = router;
