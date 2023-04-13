5dd47c0f01179641e2a47d995a95a2bcb9082e7d
DVPhong <21020375@vnu.edu.vn>
2023-04-13 22:23:33 +0700

    search product

--- controllers/ShopController.js
+++ controllers/ShopController.js
@@ -1,6 +1,10 @@
 const Product = require('../models/Product');
 const { multipleMongooseToObject } = require('../util/mongoose');
 const { mongooseToObject } = require('../util/mongoose');
+const Cart = require('../models/cart')
+var users = require('../models/User')
+
+var searchText;
 
 class ShopController {
     getHome(req, res, next) {
@@ -36,6 +40,43 @@ class ShopController {
                 });
             })
             .catch(next);
+    }
+
+    getSearch(req,res,next) {
+        // var cartProduct;
+        // if (!req.session.cart) cartProduct = null;
+        // else {
+        //     var cart = new cart(req.session.cart);
+        //     cartProduct = cart.generateArray();
+        // }
+        searchText = req.query.searchText !== undefined ? req.query.searchText : searchText;
+
+        // Product.createIndexes({}).catch(err => {
+        //     console.log(err);
+        // })
+        // Product.find({
+        //     $text: {$search: searchText}
+        // })
+        //  .countDocuments()
+        //  .then(numProduct => {
+        //     totalItems = numProduct
+        //     return Product.find({$text : {$search:searchText}})
+         //.skip((page - 1) *12)
+         //.limit(12);
+        //})
+          
+        Product.find({name:{$regex:searchText}})
+        .then(products => {
+            res.render('search',{
+                title: 'Kết quả tìm kiếm cho' + searchText,
+                user:req.user,
+                searchProducts: products,
+                searchT:searchText,
+                //cartProduct:cartProduct
+            })
+        })
+        .catch(err =>{console.log(err)})   
+        //res.render('search')     
     }
 }
 
